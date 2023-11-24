717a5e3b1cbee554b2e68fef4a0b823cd56d9640
Megha Mahajan <98259920+MegMahajan@users.noreply.github.com>
2023-11-24 12:43:50 +0530

    Update DeployArtifactory.groovy

--- vars/DeployArtifactory.groovy
+++ vars/DeployArtifactory.groovy
@@ -1,6 +1,6 @@
 def call() {
     def artifactoryUrl = 'http://54.210.121.167:8082/artifactory/example-repo-local/'
-    def jarPath = "/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+    def jarFileName = "kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
     def username = 'admin'
     def password = 'Admin@123' // Replace with your Artifactory password
 
